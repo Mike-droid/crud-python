ffe422cec08a4fce096c9fe2bb971bb0a16392d1
Miguel Ángel Reyes Moreno <miguelreyesmoreno@hotmail.com>
2021-05-11 20:11:33 -0500

    Terminado modulo de strings y ciclos

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 clients = 'Miguel,Daniel,'
 
 
@@ -17,28 +19,99 @@ def list_clients():
   print(clients)
 
 
+def update_client(client_name, updated_client_name):
+  global clients
+
+  if client_name in clients:
+    clients = clients.replace(client_name + ',' , updated_client_name)
+  else:
+    client_not_found()
+
+
+def delete_client(client_name):
+  global clients
+
+  if client_name in clients:
+    clients = clients.replace(client_name + '+' , '')
+  else:
+    client_not_found()
+
+
+def search_client(client_name):
+  clients_list = clients.split(',')
+
+  for client in clients_list:
+    if client != client_name:
+      continue
+    else:
+      return True #return siempre termina la ejecución de una función
+
+
 def _add_comma():
   global clients
 
   clients += ','
 
 
+def client_not_found():
+  print('Client is not in clients list')
+
+
 def _print_welcome():
   print('WELCOME TO PLATZI SALES')
   print('*' * 50)
   print('[C]reate client')
+  print('[R]ead clients')
+  print('[U]pdate client')
   print('[D]elete client')
+  print('[S]earch client')
+  print('[E]xit program')
+
+
+def _get_client_name():
+  client_name = None
+
+  while not client_name:
+    client_name = input('What is the client name?: ')
+    if client_name == 'exit':
+      client_name = None
+      break
+  if not client_name:
+    sys.exit()
+  return client_name
 
 
 if __name__ == '__main__':
   _print_welcome()
 
   command = input('What would you like to do today?: ')
-  if command == 'C' or command == 'c':
-    client_name = input('What is the client name?: ')
+  command = command.upper()
+
+
+  if command == 'C':
+    client_name = _get_client_name()
     create_client(client_name)
     list_clients()
-  elif command == 'D' or command == 'd':
-    pass
+  elif command == 'R':
+    list_clients()
+  elif command == 'U':
+    client_name = _get_client_name()
+    updated_client_name = input('What is the updated client name?: ')
+    update_client(client_name, updated_client_name)
+    list_clients()
+  elif command == 'D':
+    client_name = _get_client_name()
+    delete_client(client_name)
+    list_clients()
+  elif command == 'S':
+    client_name = _get_client_name()
+    found = search_client(client_name)
+
+    if found:
+      print('the client is in the client\'s list')
+    else:
+      print(f'The client {client_name} is not in our client\'s list')
+  elif command == 'E':
+    sys.exit()
   else:
     print('Invalid command')
